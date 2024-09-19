8c2184cf8c4d44dfc26902381b3c6f2a27b78d39
Jonathen Whitford <paredes.jonathen@yahoo.com>
2024-09-19 14:56:20 -0400

    completed lab

--- lib/song.py
+++ lib/song.py
@@ -1,2 +1,48 @@
+#!/usr/bin/env python3
+
+
 class Song:
-    pass
+    count = 0 
+    artists = []
+    genres = []
+    genre_count = {}
+    artist_count = {}
+
+    def __init__(self, name, artist, genre):
+        self.name = name
+        self.artist = artist
+        self.genre = genre
+
+        Song.count += 1
+        if artist not in Song.artists:
+            Song.artists.append(artist)
+
+        if genre not in Song.genres:
+            Song.genres.append(genre)
+
+        if genre in Song.genre_count:
+            Song.genre_count[genre] += 1
+        else:
+            Song.genre_count[genre] = 1
+        if artist in Song.artist_count:
+            Song.artist_count[artist] += 1
+        else: 
+            Song.artist_count[artist] = 1 
+
+
+    @classmethod
+    def song_count(cls):
+        return cls.count
+    @classmethod
+    def all_artists(cls):
+        return cls.artists
+    @classmethod
+    def all_genres(cls):
+        return cls.genres
+
+
+song = Song("99 Problems", "Jay Z", "Rap")
+print(Song.song_count())
+print(Song.all_artists())
+print(Song.all_genres())
+    
